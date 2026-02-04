0181d4274cd56f24330b55d18d910fc62efa4e2f
AsfaaKhan <khanzadiasfi0314@gmail.com>
2026-02-05 00:58:23 +0500

    Set the backend folder for deployment

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,9 @@ if __name__ == "__main__":
         "app.huggingface_app:application",
         host="0.0.0.0",
         port=8000,
-        reload=True
+        reload=True,
+        forwarded_allow_ips="*",
+        proxy_headers=True
     )
 
 
